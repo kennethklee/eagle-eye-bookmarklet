0822e8f1260f0d9d4023d946123088f1591be9e6
Kenneth Lee <kennethkl@gmail.com>
2014-03-13 23:02:45 +0000

    completed speed reading tool

--- app/bookmarklet.js
+++ app/bookmarklet.js
@@ -18,8 +18,8 @@
     // Returns 2d array of words, their focus point, and delay until the next word
     var parse = function(str) {
         return str.trim().split(/[\s\n]+/).reduce(function(words, str) {
-            // focus point
-            focus = (str.length-1)/2|0;
+            var focus = (str.length - 1) / 2 | 0,
+                delay = 60000 / window.eagleEyeWpm;
 
             for (j = focus; j >= 0; j--) {
                 if (/[aeiou]/.test(str[j])) {
@@ -28,12 +28,11 @@
                 }
             }
 
-            delay = 60000/500 // 500 wpm
-
             // Add a delay for long words, semi-stops and full-stops.
             if (str.length > 6) delay += delay / 4;
             if (str.length > 9) delay += delay / 4;
-            if (~str.indexOf(',')) delay += delay / 2;
+            if (~str.indexOf(',')) delay += delay;
+            if (~str.indexOf('-')) delay += delay;
             if (/[\.!\?;]$/.test(str)) delay += delay * 1.5;
 
             if (str.length >= 15 || str.length - focus > 7) {
@@ -70,7 +69,7 @@
         if (window.readability) return readabilityReady();
 
         //makeEl('script', {src: '/readability.js'}, document.head);
-        makeEl('script', {src: 'https://www.squirt.io/bm/readability.js'}, document.head);
+        makeEl('script', {src: 'http://www.squirt.io/bm/readability.js'}, document.head);
         handler = document.addEventListener('readability.ready', readabilityReady);
     };
 
@@ -81,17 +80,45 @@
         div.style.height = '200px';
         div.style.top = div.style.bottom = div.style.right = div.style.left = 0;
         div.style.margin = 'auto';
-        div.style.backgroundColor = 'rgba(0,0,0,0.7)';  // Does this work?
+        div.style.backgroundColor = 'rgba(0,0,0,0.7)';  // Does this work in all browsers?
+        div.style.color = 'white';
+        div.style.fontSize = '40px';
+        div.style.textAlign = 'center';
+        div.style.lineHeight = '200px';
+        div.style.fontFamily = 'Consolas';
     };
 
     var clear = function() {
         var div = document.querySelector('#eagle-reader');
         div.parentNode.removeChild(div);
     };
+  
+    var play = function() {
+        var word = window.eagleEyeQueue.splice(0, 1)[0],
+            div = document.querySelector('#eagle-reader');
+        
+        if (word) {
+            var pre = word.word.substr(0, word.focus) || '';
+            var letter = word.word.substr(word.focus, 1);
+            var post = word.word.substr(word.focus + 1, word.word.length) || '';
+
+            if (pre.length > post.length) {
+                post += Array(pre.length - post.legnth + 1).join('&nbsp;');
+            } else if (post.length > pre.length) {
+                pre = Array(post.length - pre.length + 1).join('&nbsp;') + pre;
+            }
+            
+            div.innerHTML = pre + '<span style="color: red">' + letter + '</span>' + post;
+            setTimeout(play, word.delay);
+        } else {
+            setTimeout(clear, 1000);
+        }
+    };
 
     getText(function(text) {
         render();
-        console.log(parse(text));
-        setTimeout(clear, 5000);
+        window.eagleEyeWpm = 350;
+        window.eagleEyeQueue = parse(text);
+        setTimeout(play, 1000);
     });
 })();
